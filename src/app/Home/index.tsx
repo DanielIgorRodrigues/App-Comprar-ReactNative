45ca8b40e78bcf83e1173c8dbae4707f04d458aa
Daniel Igor Rodrigues <daniel.rodrigues@lincros.com>
2025-10-15 23:10:35 -0300

    primeiro commit

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,17 @@
+import { View, Image } from "react-native"
+import { styles } from "./styles"
+
+import { Button } from "@/components/Button";
+
+export function Home() {
+  return (
+    <>
+    <View style={styles.container}>
+      <Image source={require("@/assets/logo.png")} style={styles.logo}></Image>
+      <Button />
+
+    </View>
+    </>
+  );
+}
+
